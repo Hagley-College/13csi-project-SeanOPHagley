eeca69987d0a388530a317521356089dcd97d95d
= <=>
2023-06-26 11:22:58 +1200

    added file functionality

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import json
 from map import Map
-j = {
-    "tile_size": 32,
-    "texture_paths": ["\\assets\\textures\\default-floor.png","\\assets\\textures\\default-wall.png"],
-    "player": {"x": 1, "y":0, "texture": 0},
-    "map": [
-            [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True},{"texture": 1, "collisions": True}],
-            [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}],
-            [{"texture": 1, "collisions": True},{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}],
-            [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}]
+# j = {
+#     "tile_size": 32,
+#     "texture_paths": ["\\assets\\textures\\default-floor.png","\\assets\\textures\\default-wall.png"],
+#     "player": {"x": 1, "y":0, "texture": 0},
+#     "map": [
+#             [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True},{"texture": 1, "collisions": True}],
+#             [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}],
+#             [{"texture": 1, "collisions": True},{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}],
+#             [{"texture": 1, "collisions": True},{"texture": 0, "collisions": False},{"texture": 0, "collisions": False},{"texture": 1, "collisions": True}]
             
-            ]
-}
+#             ]
+# }
 
 
-f = open(".\\maps\\default\\map.json","w")
-f.write(json.dumps(j, indent=4))
-f.close()
+# f = open(".\\maps\\default\\map.json","w")
+# f.write(json.dumps(j, indent=4))
+# f.close()
 m = Map("default")
--- map.py
+++ map.py
@@ -1,22 +1,34 @@
 import json
+import os
 
 from vec2d import Vec2d
 from player import Player
 
-map_folder = ".\\maps\\"
+map_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),"./maps/"))
 
 class Map():
     
     
 
     def __init__(self,map_name) -> None:
-        map_file = open(map_folder + map_name + "\\map.json","r")
+        
+        map_path = os.path.normpath(os.path.join(map_folder,os.path.normpath(map_name)))
+        print(map_path)
+        map_file_path = os.path.join(map_path,"map.json") # os.path.normpath(os.path.join(map_folder,os.path.normpath(map_name+ "/map.json")))
+        map_file = open(map_file_path,"r")
         self.map_json = json.loads(map_file.read())
         map_file.close()
 
         self.tile_size = self.map_json["tile_size"]
         self.player = Player(Vec2d(self.map_json["player"]["x"],self.map_json["player"]["y"]),self.map_json["player"]["texture"])
-        textures = []
-        map = [[]]
-        print(self.player)
 
+
+        self.textures = []
+        texture_file_folder = os.path.join(map_path,"textures")
+        for texture_path in self.map_json["texture_paths"]:
+            texture_path
+        map = [[]]
+        print(self.textures)
+
+if __name__ == "__main__":
+    map = Map("default")
